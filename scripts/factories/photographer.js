95e18cafa8e819bc786e23220eb495a830bda795
Theo Puteaux <theo.ptx@laposte.net>
2022-02-01 14:27:11 +0100

    creation des informations pour le photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const link = document.createElement( 'a' );
+        link.setAttribute('href','photographer.html');
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const info = document.createElement('div'); 
+        const origin = document.createElement('p');
+        origin.className = 'origin';
+        origin.textContent = city+', '+country;
+        const tag = document.createElement('p');
+        tag.className = 'tag';
+        tag.textContent = tagline;
+        const tariff = document.createElement('p');
+        tariff.className = 'tariff'
+        tariff.textContent = price+'€/jour';
+        info.appendChild(origin)
+        info.appendChild(tag)
+        info.appendChild(tariff)
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(info);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, id, city, country, tagline, price, portrait, getUserCardDOM }
 }
