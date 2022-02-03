66d472f46b4e079cdae4847ca4932de1bf4caf5d
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-02-03 22:51:23 +0100

    ajout de la fonction UserPresentationCardDOM pour la page d'acceuil photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,37 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    // Penser à remplacer par les données récupérées dans le json
+    const response = await fetch('data/photographers.json')
+    const photographers = await response.json() 
+    console.log(photographers)
+    console.log(photographers['photographers'])
+    return ({ photographers : photographers['photographers'] })
+  
+
+}
+
+
+
+async function displayDataInfo(photographers) {
+    const photographersHeader = document.querySelector(".photograph-header");
+    const params = (new URL(document.location)).searchParams;
+    const id = params.get('id');
+    console.log(id);
+    photographers.forEach((photographer) => {
+        if(photographer.id == id){
+        console.log(photographer)
+        console.log(photographer.id)
+        const photographerModel = photographerFactory(photographer);
+        const userPresentationCardDOM = photographerModel.getUserPresentationCardDOM();
+        photographersHeader.appendChild(userPresentationCardDOM);
+        }
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    console.log('init est appele ? ');
+    const { photographers } = await getPhotographers();
+    displayDataInfo(photographers);
+};
+
+init();
