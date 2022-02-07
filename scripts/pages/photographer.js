13ef4f117b11260ba04f7c309dbbd0603f77800f
Theo Puteaux <theo.ptx@laposte.net>
2022-02-07 08:39:03 +0100

    recupere les données media pour les affichers suivant l'artiste

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,37 +1,63 @@
+const params = (new URL(document.location)).searchParams;
+const id = params.get('id');
+
+
 async function getPhotographers() {
     // Penser à remplacer par les données récupérées dans le json
     const response = await fetch('data/photographers.json')
     const photographers = await response.json() 
     console.log(photographers)
     console.log(photographers['photographers'])
+    console.log({ photographers : photographers['photographers'] })
     return ({ photographers : photographers['photographers'] })
   
 
-}
+};
 
+async function getPhotographersMedia() {
+    const response = await fetch('data/photographers.json')
+    const photographersMedia = await response.json() 
+    console.log(photographersMedia)
+    console.log(photographersMedia['media'])
+    console.log({ photographersMedia : photographersMedia['media'] })
+    return photographersMedia['media'] ;
+};
 
 
 async function displayDataInfo(photographers) {
-    const photographersHeader = document.querySelector(".photograph-header");
-    const params = (new URL(document.location)).searchParams;
-    const id = params.get('id');
-    console.log(id);
+    const photographHeader = document.querySelector(".photograph-header");
+    console.log(photographers);
     photographers.forEach((photographer) => {
         if(photographer.id == id){
         console.log(photographer)
         console.log(photographer.id)
         const photographerModel = photographerFactory(photographer);
         const userPresentationCardDOM = photographerModel.getUserPresentationCardDOM();
-        photographersHeader.appendChild(userPresentationCardDOM);
+        photographHeader.appendChild(userPresentationCardDOM);
         }
     });
 };
 
+async function displayMedia(medias) {
+      const photographersMedia = document.querySelector(".photograph-media");
+      console.log(medias);
+        medias.forEach((media) => {
+          if(media.photographerId == id){
+              console.log(media);
+              console.log(media.photographerId);
+              const mediaModel = mediaFactory(media);
+              const mediaCardDOM = mediaModel.getMediaCardDOM();
+              photographersMedia.appendChild(mediaCardDOM);
+          }
+      });
+};
+
 async function init() {
     // Récupère les datas des photographes
-    console.log('init est appele ? ');
     const { photographers } = await getPhotographers();
+    const medias = await getPhotographersMedia();
     displayDataInfo(photographers);
+    displayMedia(medias);
 };
 
 init();
