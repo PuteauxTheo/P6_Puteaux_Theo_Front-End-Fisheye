02f025a0492bb342126e4c4100425f020e3dc8f6
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-02-17 11:15:27 +0100

    insertion de la fonction sendContactInfo

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,7 +9,6 @@ async function getPhotographers() {
     const response = await fetch('data/photographers.json')
     const photographers = await response.json()
     return ({ photographers : photographers['photographers'] })
-  
 
 };
 
@@ -22,7 +21,6 @@ async function getPhotographersMedia() {
 //------- Display Info Photograph ---------//
 
 const photographMedia = document.querySelector(".photograph-media")
-
 
 async function displayDataInfo() {
     const { photographers } = await getPhotographers();
@@ -38,10 +36,7 @@ async function displayDataInfo() {
         photographHeader.appendChild(userPresentationCardDOM);
         }
     });
-
-
 };
-
 
 //------- Display Media With Sort ---------//
 
@@ -84,7 +79,6 @@ async function displayMedia() {
 
     }
       const photographersMedia = document.querySelector(".photograph-media");
-      
       
         medias.forEach((media) => {
           if(media.photographerId == id){
@@ -141,7 +135,7 @@ async function manageLikes(){
 async function displayLightBox() {
     const { medias } = await getPhotographersMedia();
 
-    const lightBoxModalContent = document.querySelector(".modal-content")
+    const lightBoxModalContent = document.querySelector(".modal-lightbox-content")
     medias.forEach(media => {
         if(media.photographerId == id){
         const lightBoxModel = lightBox(media);
@@ -149,61 +143,16 @@ async function displayLightBox() {
         lightBoxModalContent.appendChild(lightBoxDOM)
         }
     })
-
-}
-
-//------- Form Verification ---------//
-
-const firstName = document.getElementById('first');
-const lastName = document.getElementById('last');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
-const contact_modal = document.getElementById('contact_modal');
-
-
-async function IsFormCorrect(){
-    console.log("je suis dans isformcorrect")
-    firstName.addEventListener('change', function(){
-    let firstRegex = new RegExp('^[a-zA-Z][^0-9]+$');
-
-    let testFirst = firstRegex.test(this.value);
-
-    if(!testFirst || (inputFirst.value == "")){
-        console.log("veuillez rentrer un prenom correct")
-        return false;
-    }   
-        
-    return true;
-})
-
-
-
 }
 
 async function init() {
     // Récupère les datas des photographes
-    // const { photographers } = await getPhotographers();
-    // const { medias } = await getPhotographersMedia();
-    // console.log('tableau media '+medias);
-    if(IsFormCorrect()){
-        let btnSend = document.getElementsByClassName('contact_button')[0];
-        btnSend.addEventListener('click', function(e){
-            e.preventDefault();
-            console.log("j'ecoute btnSend")
-            console.log("Prenom : "+ firstName.value)
-            console.log("Nom : "+ lastName.value)
-            console.log("Email : "+ email.value)
-            console.log("Message : "+ message.value)
-            contact_modal.style.display = "none";
-            
-        })
-    }else{
-        //renvoyer des erreurs
-    }
+    
     displayDataInfo();
     displayMedia();
     manageLikes();
     displayLightBox();
+    sendContactInfo();
     
 };
 
