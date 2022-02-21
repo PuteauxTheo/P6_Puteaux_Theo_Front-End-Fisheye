5325afaa356196564933bc0c5c568b51ebc535b1
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-02-21 17:24:48 +0100

    changement du formulaire, erreur sur le click a regler

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -13,55 +13,95 @@ function closeModalContact() {
 //------- Form Verification ---------//
 
 const firstName = document.getElementById('first');
+
+firstName.addEventListener('change',function() {
+    validateName(this);
+})
+
 const lastName = document.getElementById('last');
+
+lastName.addEventListener('change',function(){
+    validateName(this);
+})
+
+async function validateName(inputName) {
+    const nameRegex = new RegExp('^[a-zA-Z][^0-9]+$');
+    console.log("inputName value : "+inputName.value)
+    let testName = nameRegex.test(inputName.value);
+    console.log(" regex value "+ testName)
+        if(!testName || (inputName.value == "")){
+            console.log("veuillez rentrer un prenom ou nom correct")
+            return false;
+        }else{
+            return true;
+        }
+}
+
 const email = document.getElementById('email');
+
+email.addEventListener('change',function() {
+    validateEmail(this);
+})
+
+async function validateEmail(inputEmail){
+    const emailRegex = new RegExp( '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
+    let testEmail = emailRegex.test(inputEmail.value);
+        if(!testEmail || (inputEmail.value == "")){
+            console.log("veuillez rentrer un email correct")
+            return false;
+        }else{
+            console.log(" email valide ")
+            return true;
+        }
+}
 const message = document.getElementById('message');
+
+message.addEventListener('change', function() {
+    valideMessage(this);
+})
+
+async function valideMessage(inputMessage){
+    const messageRegex = new RegExp('^[a-zA-Z][^0-9]+$')
+    console.log(" inputmessage value : "+ inputMessage.value)
+    let testMessage = messageRegex.test(inputMessage.value);
+    console.log(" regex value message"+ testMessage)
+        if(!testMessage || (inputMessage.value == "" )){
+            console.log("veuillez rentrer un message")
+            return false;
+        }else{
+            return true;
+        }
+}
 const contact_modal = document.getElementById('contact_modal');
 
 
 async function IsFormCorrect(){
-    console.log("je suis dans isformcorrect")
-    const nameRegex = new RegExp('^[a-zA-Z][^0-9]+$');
-    firstName.addEventListener('change', function(){
-        let testFirst = nameRegex.test(this.value);
-        if(!testFirst || (this.value == "")){
-            console.log("veuillez rentrer un prenom correct")
-            return false;
-        }else{
-            return true;
-        }
-    })
-
-    lastName.addEventListener('change', function(){
-        let testLast = nameRegex.test(this.value);
-        if(!testLast || (this.value == "")){
-            console.log("veuillez rentrer un nom correct")
-            return false;
-        }else{
-            return true;
-        }
-    })
-    const emailRegex = new RegExp( '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
-    email.addEventListener('change', function(){
-        let testLast = emailRegex.test(this.value);
-        if(!testLast || (this.value == "")){
-            console.log("veuillez rentrer un nom correct")
-            return false;
-        }else{
-            return true;
-        }
-    })
-}
-async function sendContactInfo() {
-    if(IsFormCorrect()){
-        let btnSend = document.getElementsByClassName('contact_button')[0];
-        btnSend.addEventListener('click', function(e){
-            e.preventDefault();
-            console.log("Prenom : "+ firstName.value)
-            console.log("Nom : "+ lastName.value)
-            console.log("Email : "+ email.value)
-            console.log("Message : "+ message.value)
-            contact_modal.style.display = "none";
-        })
+    console.log(" je suis dans isFormCorrect ")
+    if( validateName(firstName) & validateName(lastName) & validateEmail(email) & valideMessage(message)){
+        console.log("Prenom : "+ firstName.value)
+        console.log("Nom : "+ lastName.value)
+        console.log("Email : "+ email.value)
+        console.log("Message : "+ message.value)
+        contact_modal.style.display = "none";
     }
+    
 }
+
+const btnSend = document.getElementsByClassName('contact_button')[0];
+
+btnSend.addEventListener('click', function(e){
+    e.preventDefault();
+    IsFormCorrect(this);
+})
+
+// async function sendContactInfo() {
+//         let btnSend = document.getElementsByClassName('contact_button')[0];
+//         btnSend.addEventListener('click', function(e){
+//             e.preventDefault();
+//             console.log("Prenom : "+ firstName.value)
+//             console.log("Nom : "+ lastName.value)
+//             console.log("Email : "+ email.value)
+//             console.log("Message : "+ message.value)
+//             contact_modal.style.display = "none";
+//         })
+// }
