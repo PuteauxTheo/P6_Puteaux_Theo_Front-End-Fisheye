873fb76aa4dbc1d13bdf2c5b9d75675e04246e92
Theo Puteaux <theo.ptx@laposte.net>
2022-02-26 10:03:50 +0100

    finalisation de la lightBox

--- scripts/lightBox.js
+++ scripts/lightBox.js
@@ -38,44 +38,28 @@ async function closeModalLightbox() {
     document.getElementById("idmodal-lightbox").style.display = "none";
 }
 
-//var mediaArticle = document.querySelectorAll(".media-article");
-// var children = mediaArticle.childNodes
-// var array = Array.from(mediaArticle)
-//     console.log(array)
-//     console.log(mediaArticle.childNodes)
-    //mediaArticle.children.textContent = " test "
-    // for ( i = 0; i <= mediaArticle.length; i++){
-    //     console.log(mediaArticle)
-    //     console.log(mediaArticle[i])
-    //     mediaArticle[i].childNodes[1].onclick = currentSlide(i+1);
-    // }
-
 var slideIndex = 1;
-showSlides(slideIndex);
 
 async function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
 async function currentSlide(n) {
+    console.log(" n de curentSlide "+n)
     showSlides(slideIndex = n);
 }
 
 async function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    
-    // var dots = document.getElementsByClassName("demo");
-    // var captionText = document.getElementById("caption");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
+        console.log(" slides "+ slides[i]);
+
         slides[i].style.display = "none";
     }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
+
     slides[slideIndex-1].style.display = "block";
-    //dots[slideIndex-1].className += " active";
-    //captionText.innerHTML = dots[slideIndex-1].alt;
 }
